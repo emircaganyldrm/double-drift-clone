8b471eb6e51a22c6a1107687ca1a24bdec3f58d8
emircagan <emir.yildirim@udogames.com>
2024-10-11 19:31:36 +0300

    main scene and camera manager added

---
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager Instance;
+
+    [SerializeField] private CameraID startCamera;
+    [SerializeField] private CameraData[] cameras;
+    
+    private CameraID _currentCamera;
+    
+    private Dictionary<CameraID, Camera> _cameraDictionary = new Dictionary<CameraID, Camera>();
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+        CreateDictionary();
+    }
+
+    private void Start()
+    {
+        ChangeCamera(startCamera);
+    }
+
+    public void ChangeCamera(CameraID cameraID)
+    {
+        GetCamera(_currentCamera)?.gameObject.SetActive(false);
+        _currentCamera = cameraID;
+        GetCamera(_currentCamera).gameObject.SetActive(true);
+    }
+    
+    private Camera GetCamera(CameraID cameraID)
+    {
+        return _cameraDictionary[cameraID];
+    }
+    
+    private void CreateDictionary()
+    {
+        foreach (CameraData cameraData in cameras)
+        {
+            _cameraDictionary.Add(cameraData.cameraID, cameraData.camera);
+        }
+    }
+}
+
+[Serializable]
+public struct CameraData
+{
+    public CameraID cameraID;
+    public Camera camera;
+}
+
+public enum CameraID
+{
+    FollowCamera,
+}
