57f22895e7b80f5a780d6b9e7a1203e299c9c54c
emircagan <emircaganyldrm@gmail.com>
2024-10-12 14:17:17 +0300

    car controller improvements

--- Assets/Scripts/CarControls/CarController.cs
+++ Assets/Scripts/CarControls/CarController.cs
@@ -1,33 +1,41 @@
 using InputControls;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Car
 {
     public class CarController : MonoBehaviour
     {
-        //Movement
+        [Header("Speed Settings")]
         [SerializeField] private AnimationCurve accelerationCurve;
         [SerializeField] private AnimationCurve torqueCurve;
         [SerializeField] private float torqueMultiplier = 50;
-        [SerializeField] private float topSpeed = 70;
-        [SerializeField] private float horizontalSpeed = 7;
+        [SerializeField] private float topSpeed = 50;
+        [SerializeField] private float horizontalSpeed = 10;
         [SerializeField] private Vector2 bounds = new(-3, 3);
-        
-        //Rotation
-        [SerializeField] private Transform frontWheels;
-        [SerializeField] private Transform rearWheels;
-        [SerializeField] private float wheelMaxAngle = 45;
+
+        [Space]
+        [Header("Rotation Settings")]
+        [SerializeField] private float rotationSpeed = 5f;
+        [SerializeField] private float rotationMaxAngle = 100f;
+        [SerializeField] private float verticalRotation = .02f;
+        [SerializeField] private float horizontalRotation = .2f;
+        [SerializeField] private float wheelMaxAngle = 30;
         [SerializeField] private float wheelTurnSpeed = 900;
         
+        [Space]
+        [Header("Parts")]
+        [SerializeField] private Transform carFrame;
+        [SerializeField] private Transform frontWheels;
+        [SerializeField] private Transform rearWheels;
 
         private float _currentSpeed;
         private float _currentTorque;
-
         private Vector3 _velocity;
+        private float _currentRotation;
 
         public float SpeedProgress => _currentSpeed / topSpeed;
         public bool IsDrifting { get; private set; }
-        
 
         private void Update()
         {
@@ -44,8 +52,8 @@ namespace Car
 
         private void HandleSpeed()
         {
-            float input = InputManager.Instance.IsTapping ? 1 : -1;
-            _currentSpeed += accelerationCurve.Evaluate(_currentSpeed / topSpeed) * input * _currentTorque;
+            float input = InputManager.Instance.IsTapping ? 1 : 0;
+            _currentSpeed += accelerationCurve.Evaluate(SpeedProgress) * input * Time.deltaTime * _currentTorque;
             _currentSpeed = Mathf.Clamp(_currentSpeed, 0, topSpeed);
         }
 
@@ -70,29 +78,34 @@ namespace Car
 
         private void CalculateTorque()
         {
-            _currentTorque = torqueCurve.Evaluate(_currentSpeed / topSpeed) * torqueMultiplier * Time.deltaTime;
+            float input = InputManager.Instance.IsTapping ? 1 : -1;
+            _currentTorque += torqueCurve.Evaluate(_currentSpeed / topSpeed) * input * Time.deltaTime *
+                              torqueMultiplier;
+            _currentTorque = Mathf.Clamp(_currentTorque, 0, 100);
         }
 
         private void HandleRotation()
         {
-            float direction = Mathf.Sign(transform.position.x);
-
             bool input = InputManager.Instance.IsTapping;
-            
-            float rotation = transform.position.x / bounds.y * direction;
 
             if (input)
             {
-                Quaternion targetRotation = Quaternion.Euler(Vector3.up * (-70 * direction));
-                transform.rotation = Quaternion.Lerp(Quaternion.identity, targetRotation,
-                    rotation * Time.deltaTime * 60);
+                float swipeDelta = InputManager.Instance.SwipeDelta;
+                float targetRotationAngle = swipeDelta * -rotationMaxAngle;
+                _currentRotation =
+                    Mathf.Lerp(_currentRotation, targetRotationAngle, Time.deltaTime * rotationSpeed);
             }
             else
             {
-                Quaternion targetRotation = Quaternion.Euler(Vector3.up * 0);
-                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation,
-                    Mathf.Abs(rotation) * Time.deltaTime * 10);
+                _currentRotation = Mathf.Lerp(_currentRotation, 0, Time.deltaTime * rotationSpeed / 3);
             }
+
+            Quaternion targetRotation = Quaternion.Euler(0, _currentRotation, 0);
+            transform.rotation = targetRotation;
+
+            carFrame.localRotation = Quaternion.Lerp(carFrame.localRotation,
+                Quaternion.Euler(-_currentTorque * verticalRotation, 0, _currentRotation * horizontalRotation), Time.deltaTime * 10);
+
 
             IsDrifting = Mathf.Abs(transform.localEulerAngles.y) > 20;
         }
@@ -109,7 +122,7 @@ namespace Car
                 Vector3 fwRotation = child.eulerAngles;
                 fwRotation.y = Mathf.Clamp(InputManager.Instance.SwipeDelta * wheelMaxAngle, -wheelMaxAngle,
                     wheelMaxAngle);
-                child.eulerAngles = fwRotation;
+                child.eulerAngles = -fwRotation;
             }
 
             rearWheels.Rotate(Vector3.right * spin);
