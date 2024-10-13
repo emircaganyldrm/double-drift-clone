15d12554497efadaec9c71d7d46117abdd89ea76
emircagan <emircaganyldrm@gmail.com>
2024-10-13 18:18:54 +0300

    car crash handling

---
+++ Assets/Scripts/CarControls/CarCrashHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Car
+{
+    public class CarCrashHandler : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody rigidbodyPrefab;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out TrafficCarController _))
+            {
+                Rigidbody newPrefab = Instantiate(rigidbodyPrefab, transform.position, transform.rotation);
+                newPrefab.AddForceAtPosition(new Vector3(0,1,10),transform.position);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
